cf30d7d040b204dfb37f74b02b5a0fdda849a9bc
chepisan <43737010+chepisan@users.noreply.github.com>
2018-10-03 12:30:01 -0500

    Update test.sh

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -8,7 +8,7 @@ echo '"cross-env" dependency to be installed as "devDependencies". For the'
 echo 'purposes of this tutorial, this flag is not important. However, when'
 echo 'installing this dependency, it would typically be done so using this'
 echo 'flag. For a comprehensive explanation about "devDependencies", see'
-echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
+echo 'httpss://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
 set -x
 # npm install --save-dev cross-env
 set +x
